8d091d41779147aeae75e80e0d75a663d9820ca8
danger-calzone <knope2015@gmail.com>
2023-11-01 22:09:34 -0700

    Update some dashboard styles per feedback

--- app/containers/Dashboard/BobbinsWrapper.js
+++ app/containers/Dashboard/BobbinsWrapper.js
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 
 const BobbinsWrapper = styled.div`
-  background-color: #fcbed7;
-  border-radius: 7px;
   display: flex;
   padding: 10px;
   justify-content: space-between;
