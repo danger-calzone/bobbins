f9c026989373672279e7e515b832d5eae0139b77
danger-calzone <knope2015@gmail.com>
2024-01-20 17:17:31 -0800

    fix typo

--- app/containers/BobbinPage/constants.js
+++ app/containers/BobbinPage/constants.js
@@ -1,3 +1,3 @@
 export const FETCH_BOBBIN = 'bobbin/BobbinPage/FETCH_BOBBIN';
 export const FETCH_BOBBIN_FAILURE = 'bobbin/BobbinPage/FETCH_BOBBIN_FAILURE';
-export const FETCH_BOBBIN_SUCCESS = 'bobbin/BobbinPage/LFETCH_BOBBIN_SUCCESS';
+export const FETCH_BOBBIN_SUCCESS = 'bobbin/BobbinPage/FETCH_BOBBIN_SUCCESS';
--- app/containers/Dashboard/constants.js
+++ app/containers/Dashboard/constants.js
@@ -1,3 +1,3 @@
 export const FETCH_BOBBINS = 'bobbins/Dashboard/FETCH_BOBBINS';
 export const FETCH_BOBBINS_FAILURE = 'bobbins/Dashboard/FETCH_BOBBINS_FAILURE';
-export const FETCH_BOBBINS_SUCCESS = 'bobbins/Dashboard/LFETCH_BOBBINS_SUCCESS';
+export const FETCH_BOBBINS_SUCCESS = 'bobbins/Dashboard/FETCH_BOBBINS_SUCCESS';
