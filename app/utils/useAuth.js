afa97a0fbc15dfef3c76c1c6a96945974cad7d94
danger-calzone <knope2015@gmail.com>
2024-07-06 21:16:04 -0700

    update useAuth hook to check for token expiration

--- app/utils/useAuth.js
+++ app/utils/useAuth.js
@@ -1,7 +1,25 @@
+import jwt from 'jsonwebtoken';
+import { useNavigate } from 'react-router-dom';
+
 export function useAuth() {
+  const navigate = useNavigate();
+
   const isAuthenticated = () => {
     const token = window.localStorage.getItem('session');
-    return !!JSON.parse(token);
+    const parsedToken = JSON.parse(token);
+    if (!parsedToken) return false;
+
+    const decodedToken = jwt.decode(parsedToken);
+    if (!decodedToken || !decodedToken.exp) return false;
+
+    const currentDate = Date.now();
+    const exp = decodedToken.exp * 1000;
+    const isValid = currentDate < exp;
+    if (!isValid) {
+      window.localStorage.removeItem('session');
+      navigate('/login');
+    }
+    return isValid;
   };
 
   return isAuthenticated();
