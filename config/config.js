652be8c317963b0c294291aa4150a9739a2b04c9
danger-calzone <knope2015@gmail.com>
2025-09-01 21:57:40 -0700

    Fix deployment issue

---
+++ config/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'postgres',
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // important for Neon
+      },
+    },
+  },
+};
