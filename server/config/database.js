edb4a247314db57e4eaabd3595154d8bb1eab5fa
danger-calzone <knope2015@gmail.com>
2025-09-24 19:37:39 -0700

    Fix config typo

--- server/config/database.js
+++ server/config/database.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === 'production') {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSORD,
+    process.env.DB_PASSWORD,
     {
       host: process.env.DB_HOST || 'localhost',
       port: process.env.DB_PORT || 5432,
