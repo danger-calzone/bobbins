add77574edaa264210c4426ccfdcf3e8dbdde530
danger-calzone <knope2015@gmail.com>
2025-10-01 19:25:08 -0700

    temp log to debug qa environment

--- server/models/index.js
+++ server/models/index.js
@@ -6,6 +6,8 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(`${__dirname}/../../config/config.js`)[env];
 
+console.log('Using ENV:', env);
+
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
